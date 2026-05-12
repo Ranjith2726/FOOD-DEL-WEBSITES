e57dc9da7e66a264c9994a6937a5c0f8ecf3b40f
Ranjith Dumpalapalli <ranjith2726@gmail.com>
2026-05-12 19:41:24 +0530

    remove tracked files

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
